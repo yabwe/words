5ce8742d8eb8e7bf069428bca92e8cfadfeba855
Nate Mielnik <nathan@outlook.com>
2015-07-01 00:38:02 -0400

    Toggling via custom bold + italic + underline is working :boom:

--- words.js
+++ words.js
@@ -13,25 +13,53 @@ var Util = {
 
 
 var Document = function (root) {
+	this.chars = [];
 	if (!root) {
-		this.blocks = [new Block()];
+		this.blocks = [new Block(null, this)];
 	} else {
 		this.blocks = [];
 		Array.prototype.slice.call(root.childNodes).forEach(function (element) {
-			this.blocks.push(new Block(element));
+			var block = new Block(element, this);
+			this.chars = this.chars.concat(block.getChars());
+			this.blocks.push(block);
 		}, this);
 	}
 }
 
 Document.prototype = {
+	execAction: function (action, selection) {
+		var nextWord;
+		if (this.chars[selection.start]) {
+			for (var i = selection.start; i < this.chars.length && i < selection.end; i++) {
+				var next = this.chars[i];
+				if (next.parent !== nextWord) {
+					nextWord = next.parent;
+					if (nextWord) {
+						nextWord.toggleProp(action);
+					}
+				}
+			}
+		}
+	},
+
 	toString: function () {
 		return this.blocks.join('\r\n');
+	},
+
+	toHTML: function () {
+		var str = '';
+		this.blocks.forEach(function (block) {
+			str += block.toHTML();
+		}, this);
+		return str;
 	}
 }
 
 
 
-var Block = function (root) {
+var Block = function (root, parent) {
+	this.parent = parent;
+
 	this.words = [];
 	if (!root) {
 		return;
@@ -51,17 +79,37 @@ var Block = function (root) {
 		}
 		if (node.nodeType === 3) {
 			var parts = node.nodeValue.split(' ');
+			var preSpace = '';
 			parts.forEach(function (part) {
-				this.words.push(new Word(part));
+				if (part === '') {
+					preSpace += ' ';
+				} else {
+					this.words.push(new Word(preSpace + part, this));
+					preSpace = '';
+				}
 			}, this);
+			if (preSpace != '') {
+				this.words.push(new Word(preSpace, this));
+			}
 		} else {
-			this.words.push(new Word('<' + node.nodeName + '>'));
+			this.words.push(new Word('<' + node.nodeName + '>', this));
 		}
 	}
 }
 
 Block.prototype = {
 	type: 'p',
+
+	getChars: function () {
+		var chars = [];
+		this.words.forEach(function (word, index) {
+			chars = chars.concat(word.getChars());
+			if (index !== (this.words.length - 1)) {
+				chars.push(' ');
+			}
+		}, this);
+		return chars;
+	},
 
 	toString: function () {
 		var str = '<' + this.type + '>';
@@ -71,27 +119,104 @@ Block.prototype = {
 			str += this.words.join(' ');
 		}
 		return str + '</' + this.type + '>';
+	},
+
+	toHTML: function () {
+		var content = '';
+		this.words.forEach(function (word, index) {
+			content += word.toHTML();
+			if (index !== (this.words.length - 1)) {
+				content += ' ';
+			}
+		}, this);
+		return '<' + this.type + '>' + content + '</' + this.type + '>';
 	}
 }
 
 
-var Word = function (text) {
-	this.chars = (text) ? Array.prototype.slice.call(text) : [];
+var Word = function (text, parent) {
+	this.parent = parent;
+
+	this.chars = [];
+	var chars = (text) ? Array.prototype.slice.call(text) : [];
+	chars.forEach(function (char) {
+		this.chars.push(new Char(char, this));
+	}, this);
 }
 
 Word.prototype = {
+
+	toggleProp: function (prop) {
+		var setTrue = false;
+		this.chars.forEach(function (next) {
+			if (!next.props[prop]) {
+				next.props[prop] = true;
+				setTrue = true;
+			}
+		});
+
+		if (!setTrue) {
+			this.chars.forEach(function (next) {
+				next.props[prop] = false;
+			});
+		}
+	},
+
 	toString: function () {
 		return this.chars.join('');
+	},
+
+	getChars: function () {
+		return this.chars;
+	},
+
+	toHTML: function () {
+		var tags = {},
+			pre = '',
+			post = '',
+			content = '';
+		this.chars.forEach(function (next) {
+			next.getProps().forEach(function (prop) {
+				tags[prop] = true;
+			});
+			content += next.toHTML();
+		}, this);
+		Object.keys(tags).forEach(function (tag) {
+			pre = '<' + tag + '>' + pre;
+			post = post + '</' + tag + '>';
+		});
+		return pre + content + post;
 	}
 }
 
 
-var Char = function () {
-	this.char = '';
+var Char = function (char, parent) {
+	this.parent = parent;
+
+	this.char = char || '';
+	this.props = {};
 }
 
 Char.prototype = {
 	toString: function () {
+		var str = this.char;
+		this.getProps().forEach(function (prop) {
+			str += '(' + prop + ')';
+		}, this);
+		return str;
+	},
+
+	getProps: function () {
+		var props = [];
+		Object.keys(this.props).forEach(function (prop) {
+			if (this.props[prop]) {
+				props.push(prop);
+			}
+		}, this);
+		return props;
+	},
+
+	toHTML: function () {
 		return this.char;
 	}
 }
@@ -114,8 +239,22 @@ var Words = function (selector) {
 Words.prototype = {
 
 	onToolbarButtonClick: function (event) {
-		document.execCommand(event.currentTarget.getAttribute('data-action'), null, false);
-		this.updateState(this.element);
+		var target = event.currentTarget;
+		if (target.hasAttribute('data-custom-action')) {
+			this.execCustomAction(target.getAttribute('data-custom-action'));
+		} else {
+			document.execCommand(event.currentTarget.getAttribute('data-action'), null, false);
+			this.updateState(this.element);
+		}
+	},
+
+	execCustomAction: function (action) {
+		var selection = this.exportSelection(this.element);
+		document.getElementById('previous-state').value = this.doc.toString();
+		this.doc.execAction(action, selection);
+		this.element.innerHTML = this.doc.toHTML();
+		this.importSelection(selection, this.element);
+		document.getElementById('new-state').value = this.doc.toString();
 	},
 
 	updateState: function (element) {
@@ -127,23 +266,76 @@ Words.prototype = {
 
 	onInput: function (event) {
 		this.updateState(event.currentTarget);
+	},
 
-		/*
-		var x = null;
-		var treeWalker = document.createTreeWalker(event.currentTarget, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT, x, false);
-		var str = '';
-		while(treeWalker.nextNode()) {
-			var node = treeWalker.currentNode;
-			if (node.nodeType === 3) {
-				str += node.nodeValue;
-			} else {
-				if (str.length && Util.blockNames.indexOf(node.nodeName.toLowerCase()) !== -1) {
-					str += '\r\n';
-				}
-			}
-		}
-		document.getElementById('output-inner').value = str + 'END';
-		document.getElementById('output').value = event.currentTarget.textContent;*/
-		// document.getElementById('output-inner').value = event.currentTarget.innerText;
-	}
+	exportSelection: function (root) {
+        if (!root) {
+            return null;
+        }
+
+        var selectionState = null,
+            selection = document.getSelection();
+
+        if (selection.rangeCount > 0) {
+            var range = selection.getRangeAt(0),
+                preSelectionRange = range.cloneRange(),
+                start;
+
+            preSelectionRange.selectNodeContents(root);
+            preSelectionRange.setEnd(range.startContainer, range.startOffset);
+            start = preSelectionRange.toString().length;
+
+            selectionState = {
+                start: start,
+                end: start + range.toString().length
+            };
+        }
+
+        return selectionState;
+    },
+
+    importSelection: function (selectionState, root) {
+        if (!selectionState || !root) {
+            return;
+        }
+
+        var range = document.createRange();
+        range.setStart(root, 0);
+        range.collapse(true);
+
+        var node = root,
+            nodeStack = [],
+            charIndex = 0,
+            foundStart = false,
+            stop = false,
+            nextCharIndex;
+
+        while (!stop && node) {
+            if (node.nodeType === 3) {
+                nextCharIndex = charIndex + node.length;
+                if (!foundStart && selectionState.start >= charIndex && selectionState.start <= nextCharIndex) {
+                    range.setStart(node, selectionState.start - charIndex);
+                    foundStart = true;
+                }
+                if (foundStart && selectionState.end >= charIndex && selectionState.end <= nextCharIndex) {
+                    range.setEnd(node, selectionState.end - charIndex);
+                    stop = true;
+                }
+                charIndex = nextCharIndex;
+            } else {
+                var i = node.childNodes.length - 1;
+                while (i >= 0) {
+                    nodeStack.push(node.childNodes[i]);
+                    i -= 1;
+                }
+            }
+            if (!stop) {
+                node = nodeStack.pop();
+            }
+        }
+
+        var sel = document.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
 }
